cf337aab886c70966aba3a167b1762b90a52a8ee
Ethan <iamaglasswindow@gmail.com>
2025-02-18 18:09:28 +0000

    first commit

---
+++ backend/src/test/java/com/example/quickdrop/QuickdropApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickdrop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickdropApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
