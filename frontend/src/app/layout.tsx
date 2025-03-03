ab6f0bed624e6d24924cd957f2080f251fdfe353
Ethan Tsang <ethan.tsang@ibm.com>
2025-03-03 13:48:02 +0000

    Most barebones working version. Revert all changes to this commit if any issues arise. If using IntelliJ, please set pom.xml as a maven module (important!). Launch backend by navigating to backend dir and running the cmd in the terminal: ./mvnw spring-boot:run

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,16 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
-import { Geist, Azeret_Mono as Geist_Mono } from "next/font/google"
 import "./globals.css"
 
-const geistSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
-const geistMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
-})
 
 export const metadata: Metadata = {
   title: "File Dropper App",
@@ -24,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
       <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} font-sans antialiased`}>{children}</body>
+        <body>{children}</body>
       </html>
   )
 }
